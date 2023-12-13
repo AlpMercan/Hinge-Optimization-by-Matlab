20f25d773fb2875c597d8357201135204782e6ec
Alp Mercan <112685013+AlpMercan@users.noreply.github.com>
2023-12-13 22:37:07 +0300

    Update optimizeHingedistance.m

--- optimizeHingedistance.m
+++ optimizeHingedistance.m
@@ -4,12 +4,9 @@
 % Given values
 m = 2.5; % Mass of hood cover [kg]
 g = 9.81; % Gravitational acceleration [m/s^2]
-
-% Define constraints (if any)
 L_min = 0.3; 
 L_max = 0.375; 
 
-% Prepare the figure for plotting
 figure;
 hold on;
 grid on;
@@ -22,26 +19,22 @@ xlim([L_min, L_max]);
 options = optimset('Display', 'iter', 'TolX', 1e-6);
 [L_optimal, torque_min] = fminbnd(@(L) optimizationPlot(L, m, g), L_min, L_max, options);
 
-% Plot the optimal point
 plot(L_optimal, torque_min, 'gp', 'MarkerSize', 10, 'MarkerFaceColor', 'g');
 legend('Torque values', 'Optimal point');
 
-% Display the results
 fprintf('Optimal position L for the hinges is: %.4f m\n', L_optimal);
 fprintf('This results in a minimum required torque per hinge of: %.4f Nm\n', torque_min);
-
-% Function for optimization and plotting
 function torque = optimizationPlot(L, m, g)
-    persistent L_values torque_values % Declare persistent variables
-    if isempty(L_values) % Initialize them if they are empty
+    persistent L_values torque_values 
+    if isempty(L_values)
         L_values = [];
         torque_values = [];
     end
     
-    torque = m * g * L / 2; % Calculate the torque for the given L
-    L_values = [L_values, L]; % Update the L_values with the current L
-    torque_values = [torque_values, torque]; % Update the torque_values with the current torque
+    torque = m * g * L / 2; 
+    L_values = [L_values, L]; 
+    torque_values = [torque_values, torque]; 
     
-    plot(L_values, torque_values, 'b.-'); % Plot the current values
-    drawnow; % Update the plot immediately
+    plot(L_values, torque_values, 'b.-'); 
+    drawnow;
 end
